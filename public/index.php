a04f16dea6ee7295800d501ae62b7ae398853a1e
SereMark <seremark056@gmail.com>
2024-04-08 17:23:21 +0200

    Refactor table styling and data rendering in index.php

--- public/index.php
+++ public/index.php
@@ -1,35 +1,46 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <title>Product List</title>
+    <link rel="stylesheet" href="assets/css/shared.css">
 </head>
 <body>
     <?php
     require_once __DIR__ . '/../src/Models/ProductsModel.php';
-    $productsModel = new ProductsModel();
-    $products = $productsModel->fetchProducts();
-    try {
-        $products = $productsModel->fetchProducts();
-    } catch (Exception $e) {
-        echo "Error: " . $e->getMessage();
-        die();
-    }
-    ?>
-    <table border="0">
-        <?php
-        if (!empty($products)) {
-            echo '<tr>';
-            foreach (array_keys($products[0]) as $field) {
-                echo '<td>' . $field . '</td>';
-            }
-            echo '</tr>';
-            foreach ($products as $row) {
-                echo '<tr>';
-                foreach ($row as $item) {
-                    echo '<td>' . htmlspecialchars($item) . '</td>';
-                }
-                echo '</tr>';
-            }
+
+    function fetchProductsSafely() {
+        $productsModel = new ProductsModel();
+        try {
+            return $productsModel->fetchProducts();
+        } catch (Exception $e) {
+            error_log("Error fetching products: " . $e->getMessage());
+            return [];
         }
-        ?>
+    }
+
+    $products = fetchProductsSafely();
+    ?>
+
+    <table>
+        <thead>
+            <?php if (!empty($products)): ?>
+                <tr>
+                    <?php foreach (array_keys($products[0]) as $field): ?>
+                        <th><?php echo htmlspecialchars($field); ?></th>
+                    <?php endforeach; ?>
+                </tr>
+        </thead>
+        <tbody>
+                <?php foreach ($products as $row): ?>
+                    <tr>
+                        <?php foreach ($row as $item): ?>
+                            <td><?php echo htmlspecialchars($item); ?></td>
+                        <?php endforeach; ?>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </tbody>
     </table>
 </body>
 </html>
