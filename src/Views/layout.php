fa54b660b6779333de4bf96978791bd28d2fe5a7
SereMark <seremark056@gmail.com>
2024-04-12 12:14:58 +0200

    Update layout.php to include infoModal and connection check in the main content

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -22,7 +22,7 @@ if (DEBUG) {
 <!-- Navbar -->
 <?php include __DIR__ . '/navbar.php'; ?>
 
-<!-- Main Content with connection check -->
+<!-- Main Content with infoModal and connection check -->
 <div class="main">
     <?php include __DIR__ . '/infoModal.php'; ?>
     <?php include __DIR__ . '/connection.php'; ?>
@@ -34,10 +34,10 @@ if (DEBUG) {
 
 <!-- Scripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="assets/js/ajax-handler.js?v=1.0"></script>
+<script src="/assets/js/ajax-handler.js?v=1.0"></script>
 
 <script> var showModalAfterSeconds = <?php echo SHOW_MODAL_AFTER_SECONDS; ?>; </script>
-<script src="assets/js/modal.js?v=1.0"></script>
+<script src="/assets/js/modal.js?v=1.0"></script>
 
 </body>
 </html>
